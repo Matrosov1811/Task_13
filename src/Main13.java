a612e49e90843d2d0fb86c59743bc36ab5bdd8a2
Pavel <89654104202@mail.ru>
2021-09-21 12:08:37 +0300

    Исправление

--- src/Main13.java
+++ src/Main13.java
@@ -5,7 +5,7 @@ public class Main13 {
     public static void main(String[] args) {
         System.out.println("Введите любое предложение. Например: “I love java 8 Я люблю java 14 core1”");
         Scanner s = new Scanner(System.in);
-        String str = s.nextLine().replaceAll("([а-яА-Я0-9]+\\s)|([а-яА-Я0-9])", "");
+        String str = s.nextLine().replaceAll("(\\w*[0-9]\\s*)|([а-яА-Я]\\s*)|(\\w*[а-яА-Я]\\w*)", "");
         System.out.println(str);
         String[] arr = (str.split(" "));
         System.out.println(arr.length);
